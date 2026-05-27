7e132bae15c1f0de2dfa4fc8ff66ffab5025a839
mailbotprem-haha <mailbotprem@gmail.com>
2026-05-27 11:35:36 +0530

    Update prisma.config.ts

--- oneatlas/prisma.config.ts
+++ oneatlas/prisma.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "prisma/config"
 
 export default defineConfig({
   datasource: {
-    url: "postgresql://neondb_owner:npg_vIRlkhB4N8tM@ep-wild-silence-ap16dnbu.c-7.us-east-1.aws.neon.tech/neondb?sslmode=require",
+    url: process.env.DATABASE_URL!,
   },
   migrations: {
     seed: "node --import tsx/esm prisma/seed.ts",
